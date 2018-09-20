213f47cbf58625eb53fa670fe15ee2636ee467c3
Kottt4 <vc@tut.by>
2018-09-20 20:22:41 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function bigFormula (stro, array){
+    var string = stro.split("");
+    var bbb = true;
+    for (var c = 0; c <(string.length-1); c++) {
+     if (string[c] === array [0]){
+         for (var j =c+1; j<(string.length); j++) {
+             if (string[j]=== array[1] && Math.round((j-c)/2)!=(j-c)/2) {
+             string[j] = "b";
+             string[c] = "b";
+             break;
+             };
+         };
+      };
+    };
+  var count =0;
+  for (var t=0; t<string.length; t++) {
+  if (string[t]=== array[0] || string[t]=== array[1]) {count++;};
+  };
+  string = string.join("");
+  if (count >0) {bbb = false;};
+  return bbb;
+  };
+  
+  
+  var yyy = true;
+  for (var j =0; j<bracketsConfig.length; j++) {
+  var b = bracketsConfig[j];
+  var yyy = bigFormula (str, b);
+  if (yyy === false) {return yyy};
+  };
+  return yyy;
+  
+  
 }
